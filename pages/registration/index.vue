dbdedd39f3fbcee165039eb3670faf611e45c2d8
Artem Zhuravlev <an3wers@gmail.com>
2022-11-01 10:50:21 +0500

    test deploy

---
+++ pages/registration/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="registration-page bg-slate-150 -mb-20 border-b border-gray-300">
+    <div class="container">
+      <div class="grid grid-cols-12">
+        <div
+          class="col-start-1 col-span-12 md:col-start-2 md:col-span-10 lg:col-start-4 lg:col-span-6 xl:col-start-5 xl:col-span-4 pt-8 pb-16"
+        >
+          <!-- Кнопка назад -->
+          <app-go-back />
+          <div class="w-full bg-white rounded-2xl py-8 px-6 shadow-md">
+            <div class="text-2xl font-bold">Зарегистрироваться</div>
+            <div class="pt-8">
+              <registration-form />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import RegistrationForm from '@/components/forms/RegistrationForm.vue';
+import AppGoBack from '@/components/AppGoBack.vue';
+</script>
