546a3d05d42dd9b799c71a03ff904df17769f5c6
Artem Zhuravlev <an3wers@gmail.com>
2022-11-07 22:50:26 +0500

    https

--- composables/config.js
+++ composables/config.js
@@ -2,7 +2,7 @@ export const useConfig = () => {
   return {
     // API_BASE_URL: 'https://isantur.ru/',
     API_ADMIN: 'http://10.10.10.77:5168/',
-    API_BASE_URL: 'http://testv.santur.ru/',
+    API_BASE_URL: 'https://testv.santur.ru/',
     // API_ADMIN: 'https://testv.santur.ru/',
     PHONE_SITE: '73432700404',
     EMAIL_SITE: 'santur@santur.ru',
